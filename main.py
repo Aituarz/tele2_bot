58f37dd84cba7ebe9c1ff0d0b02ac3c80d5f8d8e
Aituarz <36506361+Aituarz@users.noreply.github.com>
2018-02-21 17:56:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from telegram import ReplyKeyboardMarkup
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
                           ConversationHandler)
 
-CHOOSING_HOME_TEAM, CHOOSING_AWAY_TEAM, FINAL = range(3)
-
 def help(bot, update):
     update.message.reply_text("HEEELP")
 
@@ -15,96 +13,7 @@ def hello(bot, update):
 
 
 
-def start(bot, update):
-	try:
-		reply_keyboard = apidata.getallleague_ls()
-		markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=False)
-		update.message.reply_text("Choose your destiny!",reply_markup=markup)
-		return CHOOSING_HOME_TEAM
-	except:
-		update.message.reply_text("Please try again from /start")
-		return FINAL
-	
-def team_home(bot, update,user_data):
-	try:
-		text = update.message.text
-		user_data['league'] = text
 
-		reply_keyboard = apidata.allcomands_ls(text)
-		markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=False)
-		update.message.reply_text("Choose home team!",reply_markup=markup)
-
-		return CHOOSING_AWAY_TEAM
-	except:
-		update.message.reply_text("wow wow something broken try again from /start")
-		return FINAL
-
-
-
-def team_away(bot, update, user_data):
-	try:
-		text = update.message.text
-		if text!='Back to main menu':
-			user_data['team_home'] = text
-
-			reply_keyboard = apidata.allcomands_ls(user_data['league'])
-			markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=False)
-			update.message.reply_text("Choose away team!",reply_markup=markup)
-
-
-			return FINAL
-		else:
-			update.message.reply_text("Return to main menu")
-			try:
-				reply_keyboard = apidata.getallleague_ls()
-				markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=False)
-				update.message.reply_text("Choose your destiny!",reply_markup=markup)
-				return CHOOSING_HOME_TEAM
-			except:
-				update.message.reply_text("Please try again from /start")
-				return FINAL
-	except:
-		update.message.reply_text("pfff something broken try again from /start")
-		return FINAL
-		
-
-def final_message(bot, update, user_data):
-	try:
-		text = update.message.text
-		if text!='Back to main menu':
-			user_data['team_away'] = text
-
-			output=apidata.mainfunc(user_data['team_home'],user_data['team_away'],user_data['league'])
-			update.effective_message.reply_photo(photo=open('test.png','rb'))
-			user_data.clear()
-			return ConversationHandler.END
-		else:
-			update.message.reply_text("Return to main menu")
-			try:
-				reply_keyboard = apidata.getallleague_ls()
-				markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=False)
-				update.message.reply_text("Choose your destiny!",reply_markup=markup)
-				return CHOOSING_HOME_TEAM
-			except:
-				update.message.reply_text("Please try again from /start")
-				return FINAL
-	except:
-		user_data.clear()
-		update.message.reply_text("bye-bye")
-		return ConversationHandler.END
-	
-def done(bot, update, user_data):
-    update.message.reply_text("Bye")
-    user_data.clear()
-    return ConversationHandler.END
-
-
-def error(bot, update, error):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
-    
-    
-    
     
 if __name__ == "__main__":
     # Set these variable to the appropriate values
