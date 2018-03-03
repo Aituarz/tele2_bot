b7e2b8bd18f572b6b678b9fb08a77a65c82ef4ec
Aituarz <36506361+Aituarz@users.noreply.github.com>
2018-03-03 16:04:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMa
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
                           ConversationHandler)
 
-VOICE_PROBLEM, INTERNET_PROBLEM, FINAL = range(3)
-
 def start(bot, update):
 	try:
     keyboard = [[InlineKeyboardButton("Voice", callback_data='1'),
@@ -13,48 +11,7 @@ def start(bot, update):
 
     update.message.reply_text('Please choose your problem:', reply_markup=reply_markup)
 		return FINAL
-	except:
-		update.message.reply_text("Please try again from /start")
-		return FINAL
-  
-def voice(bot, update):
-	try:
-    keyboard = [[InlineKeyboardButton("2G", callback_data='3'),
-                 InlineKeyboardButton("3G", callback_data='4')]]
-
-    update.message.reply_text('Please choose your technology:', reply_markup=reply_markup)
-		return FINAL
-	except:
-		update.message.reply_text("Please try again from /start")
-		return FINAL
-  
-def internet(bot, update):
-	try:
-    keyboard = [[InlineKeyboardButton("2G", callback_data='5'),
-                 InlineKeyboardButton("3G LTE", callback_data='6')]]
-
-    update.message.reply_text('Please choose your technology:', reply_markup=reply_markup)
-		return FINAL
-	except:
-		update.message.reply_text("Please try again from /start")
-		return FINAL
-
-
-def help(bot, update):
-    update.message.reply_text("Use /start to test this bot.")
-
-
-def done(bot, update, user_data):
-    update.message.reply_text("Bye bye")
-    return ConversationHandler.END
-
-
-def error(bot, update, error):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
-
-    
-  
+ 
 if __name__ == "__main__":
     # Set these variable to the appropriate values
     TOKEN = "533314669:AAF_L5ViVpyyEsdy0DKK1SshUwHUQ3bQPi8"
@@ -71,32 +28,6 @@ if __name__ == "__main__":
     # Set up the Updater
     updater = Updater(TOKEN)
     dp = updater.dispatcher
-    
-    # Add handlers
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-
-        states={
-            VOICE_PROBLEM: [MessageHandler(Filters.text,
-                                          voice,
-                                          pass_user_data=True),
-                           ],
-
-            INTERNET_PROBLEM: [MessageHandler(Filters.text,
-                                           internet,
-                                           pass_user_data=True),
-                            ],
-            FINAL: [MessageHandler(Filters.text,
-                                           voice,
-                                           pass_user_data=True),
-                            ],
-		
-        },
-
-        fallbacks=[RegexHandler('^Done$', done, pass_user_data=True)]
-    )
-
-    dp.add_handler(conv_handler)
 
     # Start the webhook
     updater.start_webhook(listen="0.0.0.0",
